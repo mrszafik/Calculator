571d446c8c7faf57b5c5d81286041329aeab2d4c
Maciej Szafrański <szafmac@gmail.com>
2017-12-13 16:43:25 +0100

    OK

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -24,5 +24,135 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private string _number1_String, _number2_String;
+        private long _result, _number1, _number2;
+        private char _chosenAction;
+        private Boolean _nextNumber = false;
+
+        private void ButtonOne_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayNumber(1);
+        }
+
+        private void ButtonTwo_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayNumber(2);
+        }
+
+        private void ButtonThree_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayNumber(3);
+        }
+
+        private void ButtonFour_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayNumber(4);
+        }
+
+        private void ButtonFive_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayNumber(5);
+        }
+
+        private void ButtonSix_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayNumber(6);
+        }
+
+        private void ButtonSeven_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayNumber(7);
+        }
+
+        private void ButtonEight_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayNumber(8);
+        }
+
+        private void ButtonNine_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayNumber(9);
+        }
+
+        private void ButtonZero_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayNumber(0);
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "";
+            _nextNumber = true;
+            _chosenAction = '+';
+        }
+
+        private void ButtonMul_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "";
+            _nextNumber = true;
+            _chosenAction = '*';
+        }
+
+        private void ButtonSubb_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "";
+            _nextNumber = true;
+            _chosenAction = '-';
+        }
+
+        private void ButtonDiv_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "";
+            _nextNumber = true;
+            _chosenAction = '/';
+        }
+
+        private void ButtonEqual_Click(object sender, RoutedEventArgs e)
+        {
+            _number1 = int.Parse(_number1_String);
+            _number2 = int.Parse(_number2_String);
+            switch (_chosenAction)
+            {
+                case ('+'):
+                    _result = _number1 + _number2;
+                    break;
+
+                case ('-'):
+                    _result = _number1 - _number2;
+                    break;
+
+                case ('*'):
+                    _result = _number1 * _number2;
+                    break;
+
+                case ('/'):
+                    _result = _number1 / _number2;
+                    break;
+                case (' '):
+                    break;
+            }
+            _chosenAction = ' ';
+            Display.Text= _result.ToString();
+            _number1 = _result;
+            _number1_String = _number1.ToString();
+            _number2_String = " ";
+        }
+
+        private void DisplayNumber(int number)
+        {
+            if (!_nextNumber)
+            {
+                _number1_String += number;
+                Display.Text = _number1_String;
+            }
+            else
+            {
+                _number2_String += number;
+                Display.Text = _number2_String;
+            }
+
+        }
     }
+
 }
